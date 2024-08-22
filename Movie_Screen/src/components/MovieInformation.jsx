86e2495c220fd3bf5d5389db0a63f0733a907fa4
YK CHEN <yk.chenn@gmail.com>
2024-08-22 22:30:39 +0800

    api url change

--- Movie_Screen/src/components/MovieInformation.jsx
+++ Movie_Screen/src/components/MovieInformation.jsx
@@ -10,7 +10,6 @@ function MovieInformation() {
 	useEffect(() => {
 		const fetchMovies = async () => {
 			try {
-				// const response = await axios.get('http://localhost/Movie_Project/Movie/public/api/movieinfo');
 				const response = await axios.get('http://127.0.0.1:8000/api/movieinfo');
 				setMovies(response.data);
 			} catch (error) {
