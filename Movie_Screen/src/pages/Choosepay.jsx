c806f08a10b81650771c23e3b852f13641d0c04f
wei-xin-Liu <std92073@gmail.com>
2024-08-26 23:35:16 +0800

    最新公告增加，細微調整

--- Movie_Screen/src/pages/Choosepay.jsx
+++ Movie_Screen/src/pages/Choosepay.jsx
@@ -5,108 +5,103 @@ import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 
 function Choosepay() {
-	const { state } = useLocation();
+  const { state } = useLocation();
 
-	let itemDescArray = '';
-	const { selectedFoods, ticketCounts, totalPrice } = state;
-	const entries = Object.entries(ticketCounts);
-	const filteredEntries = entries.filter(([key, value]) => value > 0);
-	filteredEntries.map(([key, value]) => {
-		// "adult":1,"student":0,"early":0,"love":0
-		if (key == "adult") {
-			key = "全票"
-		}
-		else if (key == "student") {
-			key = "學生票"
-		}
-		else if (key == "early") {
-			key = "早鳥票"
-		}
-		else if (key == "love") {
-			key = "愛心票"
-		}
-		return (itemDescArray += `${key}x${value}, \n`);
-	});
+  let itemDescArray = '';
+  const { selectedFoods, ticketCounts, totalPrice } = state;
+  const entries = Object.entries(ticketCounts);
+  const filteredEntries = entries.filter(([key, value]) => value > 0);
+  filteredEntries.map(([key, value]) => {
+    // "adult":1,"student":0,"early":0,"love":0
+    if (key == 'adult') {
+      key = '全票';
+    } else if (key == 'student') {
+      key = '學生票';
+    } else if (key == 'early') {
+      key = '早鳥票';
+    } else if (key == 'love') {
+      key = '愛心票';
+    }
+    return (itemDescArray += `${key}x${value}, \n`);
+  });
 
+  for (let index = 0; index < selectedFoods.length; index++) {
+    itemDescArray += `${selectedFoods[index].name}x${selectedFoods[index].quantity}, \n`;
+  }
 
-	for (let index = 0; index < selectedFoods.length; index++) {
-		itemDescArray += `${selectedFoods[index].name}x${selectedFoods[index].quantity}, \n`;
-	}
+  const totalPricestr = totalPrice.toString();
 
+  const [paymentMethod, setPaymentMethod] = useState('online');
+  const [data, setData] = useState([]);
 
-	const totalPricestr = totalPrice.toString();
+  const navigate = useNavigate();
+  const token = localStorage.getItem('ACCESS_TOKEN'); // Check if user is logged in
 
-	const [paymentMethod, setPaymentMethod] = useState('online');
-	const [data, setData] = useState([]);
+  const saveBooking = useMutation({
+    mutationFn: async (orderdata) => {
+      try {
+        const response = await axios.post(
+          'http://127.0.0.1:8000/api/member-order',
+          orderdata,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+        console.log('Response:', response);
+        return response.data;
+      } catch (error) {
+        console.error(
+          'Axios error:',
+          error.response ? error.response.data : error.message,
+        );
+        throw error;
+      }
+    },
+  });
 
-	const navigate = useNavigate();
-	const token = localStorage.getItem('ACCESS_TOKEN'); // Check if user is logged in
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // 阻止表單默認提交行為
 
-	const saveBooking = useMutation({
-		mutationFn: async (orderdata) => {
-			try {
-				const response = await axios.post(
-					'http://127.0.0.1:8000/api/member-order',
-					orderdata,
-					{
-						headers: {
-							Authorization: `Bearer ${token}`,
-							'Content-Type': 'application/json',
-						},
-					}
-				);
-				console.log('Response:', response);
-				return response.data;
-			} catch (error) {
-				console.error(
-					'Axios error:',
-					error.response ? error.response.data : error.message
-				);
-				throw error;
-			}
-		},
-	});
+    const order = JSON.parse(localStorage.getItem('order'));
+    if (!order) {
+      console.error('Order orderdata is missing');
+      return;
+    }
 
-	const handleSubmit = async (e) => {
-		e.preventDefault(); // 阻止表單默認提交行為
+    console.log(order);
 
-		const order = JSON.parse(localStorage.getItem('order'));
-		if (!order) {
-			console.error('Order orderdata is missing');
-			return;
-		}
+    const { totalPrice, ...details } = order;
+    console.log(details);
+    const orderdata = {
+      detail: JSON.stringify(details),
+      totalPrice: parseInt(totalPrice, 10), // Ensure totalPrice is an integer
+    };
 
-		console.log(order);
+    console.log('Orderdata to be sent:', orderdata); // Log the orderdata for debugging
+    saveBooking.mutate(orderdata); // Trigger the mutation
 
-		const { totalPrice, ...details } = order;
-		console.log(details);
-		const orderdata = {
-			detail: JSON.stringify(details),
-			totalPrice: parseInt(totalPrice, 10), // Ensure totalPrice is an integer
-		};
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/bluepay', {
+        itemDescArray,
+        totalPricestr,
+      });
 
-		console.log('Orderdata to be sent:', orderdata); // Log the orderdata for debugging
-		saveBooking.mutate(orderdata); // Trigger the mutation
+      setData(response.data); // 保存數據到狀態
+      console.log(response.data);
+      navigate('/Bluepay', { state: { data: response.data } });
 
-		try {
-			const response = await axios.post('http://127.0.0.1:8000/api/bluepay', {
-				itemDescArray,
-				totalPricestr,
-			});
+      // 立即導航到新頁面，並傳遞數據
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-			setData(response.data); // 保存數據到狀態
-			console.log(response.data);
-			navigate('/Bluepay', { state: { data: response.data } });
-
-			// 立即導航到新頁面，並傳遞數據
-		} catch (error) {
-			console.error('Error fetching data:', error);
-		}
-	};
-
-	return (
-		<div
-			className={`
+  return (
+    <div
+      className={`
           m-auto
           w-full
           md:w-1/3
@@ -117,28 +112,28 @@ function Choosepay() {
           shadow-lg
           transition-opacity duration-500 ease-in-out
         `}
-		>
-			<h2 className='text-2xl font-bold mb-4'>付款資訊</h2>
-			<form onSubmit={handleSubmit} className='space-y-4'>
-				<div>
-					<label htmlFor='payment-method' className='block font-medium mb-2'>
-						付款方式
-					</label>
-					<div className='flex'>
-						<div className='mx-auto'>
-							<input
-								type='radio'
-								id='online-payment'
-								name='payment-method'
-								value='online'
-								checked={paymentMethod === 'online'}
-								onChange={(e) => setPaymentMethod(e.target.value)}
-								className='mr-2'
-							/>
-							<label htmlFor='online-payment'>線上付款</label>
-							<label htmlFor='online-payment'>線上付款</label>
-						</div>
-						{/* <div className='ml-[20%]'>
+    >
+      <h2 className="text-2xl font-bold mb-4">付款資訊</h2>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label htmlFor="payment-method" className="block font-medium mb-2">
+            付款方式
+          </label>
+          <div className="flex">
+            <div className="mx-auto">
+              <input
+                type="radio"
+                id="online-payment"
+                name="payment-method"
+                value="online"
+                checked={paymentMethod === 'online'}
+                onChange={(e) => setPaymentMethod(e.target.value)}
+                className="mr-2"
+              />
+              <label htmlFor="online-payment">線上付款</label>
+              <label htmlFor="online-payment">線上付款</label>
+            </div>
+            {/* <div className='ml-[20%]'>
 							<input
 								type='radio'
 								id='in-person-payment'
@@ -150,12 +145,12 @@ function Choosepay() {
 							/>
 							<label htmlFor='in-person-payment'>臨櫃付款</label>
 						</div> */}
-					</div>
-				</div>
+          </div>
+        </div>
 
-				<button
-					type='submit'
-					className='
+        <button
+          type="submit"
+          className="
                         bg-[#0466c8]
                         hover:bg-[#0466c8]/[0.8]                   
                         bg-[#0466c8]
@@ -163,16 +158,17 @@ function Choosepay() {
                         font-semibold
                         text-white
                         text-white
+                        text-white
                         py-2
                         px-4
                         rounded
-                    '
-				>
-					確認付款
-				</button>
-			</form>
-		</div>
-	);
+                    "
+        >
+          確認付款
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Choosepay;
